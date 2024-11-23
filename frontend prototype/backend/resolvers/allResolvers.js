9033b3b7e92fc740d6e4c5ed5782e196338aa848
VladimirDemin <95424460+VladimirDemin@users.noreply.github.com>
2024-11-23 20:08:48 +0300

    Временно удалён до решения проблем с зависимостями

--- frontend prototype/backend/resolvers/allResolvers.js
+++
@@ -1,9 +0,0 @@
-const authResolvers = require('./auth');
-const routeResolvers = require('./route');
-const orderResolvers = require('./order');
-
-module.exports = {
-  ...authResolvers,
-  ...routeResolvers,
-  ...orderResolvers
-};
